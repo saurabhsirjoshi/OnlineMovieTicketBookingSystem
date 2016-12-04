60a74a85b6cfdd3234bc9a16db6657e6e014af56
saurabhsirjoshi <saurabhsirjoshi@email.arizona.edu>
2016-12-04 00:06:45 -0700

    Updates to TaskController and Fascade to include UI

--- src/omtbs/core/OMTBSFascade.java
+++ src/omtbs/core/OMTBSFascade.java
@@ -4,19 +4,34 @@ import java.util.Date;
 
 public class OMTBSFascade {
 	long sessionID;
-	
+	String userName;
+	//Create a new session for new user
 	public long getSessionID() {
 		Date d = new Date();
 		sessionID = d.getTime();
 		return sessionID;
 	}
-	
+	//Validate user
 	public boolean loginUser(String uName, String pwd) {
+		if (uName.equals("TestUser") && pwd.equals("Test!23")) {
+			this.userName = uName;
+			return true;
+		}
+		else
+			return false;
+	}
+	//Log user out and invalidate session
+	public boolean logoutUser() {
+		sessionID = 0;
 		return true;
 	}
-	// Procedure to contact Adapter and book tickets
-	public void bookTickets() {
+	// Procedure to book tickets
+	public void bookTickets(String aMovie, String aTheatre, int aNoOfSeats, String aDate) {
 		
+	}
+	
+	public String getMoviesList() {
+		return "";
 	}
 	
 	// Procedure to contact DB for history
--- src/omtbs/core/TaskHandler.java
+++ src/omtbs/core/TaskHandler.java
@@ -1,5 +1,75 @@
 package omtbs.core;
 
-public class TaskHandler {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Date;
 
+public class TaskHandler {
+	public static void main(String[] args) throws IOException {
+		//Declare variables
+		String userName, passWord, movie, theatre, tempInput, bookingDate;
+		int noOfSeats;
+		boolean loopcondition;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//Create a Fascade object
+		OMTBSFascade fasc = new OMTBSFascade();
+		
+		//Begin
+		System.out.println("Welcome to the Online Movie Ticket Booking System!");
+		//Get user details:
+		System.out.print("Enter username: ");
+		userName = br.readLine();
+		System.out.print("Enter password: ");
+		passWord = br.readLine();
+		//Create session
+		long sessionID = fasc.getSessionID();
+		
+		//On successful Login the user to use OMTBS, perform tasks
+		if (fasc.loginUser(userName, passWord)) {
+			loopcondition = true;
+			while (loopcondition) {
+				tempInput = "";
+				System.out.println("**************************************************");
+				System.out.println("Welcome to the Online Movie Ticket Booking System!");
+				System.out.println("Hi " + userName +"!");
+				System.out.println("Menu:");
+				System.out.println("1) Book Tickets");
+				System.out.println("2) View History");
+				System.out.println("3) Show current list of movies");
+				System.out.println("4) Logout");
+				System.out.print("Enter choice: ");
+				tempInput = br.readLine();
+				switch(tempInput) {
+					case "1":
+						System.out.print("Enter Movie ID: ");
+						movie = br.readLine();
+						System.out.print("Enter Theatre ID: ");
+						theatre = br.readLine();
+						System.out.print("Enter Number of seats: ");
+						noOfSeats = Integer.parseInt(br.readLine());
+						System.out.print("Enter Movie Date (MM/DD/YY): ");
+						bookingDate = br.readLine();
+						fasc.bookTickets(movie,theatre, noOfSeats, bookingDate);
+						break;
+					case "2":
+						fasc.getHistory();
+						break;
+					case "3":
+						fasc.getMoviesList();
+						break;
+					case "4":
+						fasc.logoutUser();
+						loopcondition = false;
+						System.out.print("Log off successful!");
+						break;
+					default:
+						System.out.print("Invalid Input! Please re-enter!");
+						break;
+				}
+			}
+		}
+		else
+			System.out.println("Invalid user!");
+	}
 }
